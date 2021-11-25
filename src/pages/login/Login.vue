de1bee62f87d643a2d88ea6a07fa4a5dd326e180
zqc11 <46465484+zqc11@users.noreply.github.com>
2021-11-25 14:58:16 +0800

    code in github

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
+    <Header/>
       <h2>hello world</h2>
+      <Footer/>
   </div>
 </template>
 
 <script>
+import Header from '../../components/Header1.vue';
+import Footer from '../../components/Footer1.vue';
 export default {
     name: 'Login',
+    components: {
+        Header,
+    },
     data(){
         return {
-
+            
         };
     }
 }
